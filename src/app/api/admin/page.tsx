636abc77643d5eaf83d50e7cfbb66ac0f8dda69f
akuma-code <akumapl86@gmail.com>
2024-04-24 18:50:16 +0300

    sync

--- src/app/api/admin/page.tsx
+++ src/app/api/admin/page.tsx
@@ -4,13 +4,16 @@ import { Stp } from "@prisma/client";
 import { FormCreate } from "./FormCreate";
 import StpListView from "./StpList";
 import DtoStpList from "./DtoListView";
-import { Suspense } from "react";
+import { FormEvent, Suspense } from "react";
 
 
 function AministratorPage() {
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+
+    }
     return (
         <Box>
-            <form target="/api/db/saved" method="POST">
+            <form target="/api/db/saved" method="POST" onSubmit={ handleSubmit }>
                 <input type="hidden" name="limit" />
                 <button type="submit" formAction="/api/db/saved">
                     Seed db
