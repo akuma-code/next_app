bd9ea7f38792f59c90f4f2a4d62b6115c532164d
akuma-code <akumapl86@gmail.com>
2024-04-26 18:39:04 +0300

    sync

--- src/ClientComponents/LoginForm.tsx
+++ src/ClientComponents/LoginForm.tsx
@@ -17,12 +17,7 @@ export default function LoginForm() {
                 </h1>
                 <div className="w-full ">
                     <div className='flex gap-4 flex-row'>
-                        <label
-                            className="mb-3 mt-5 ml-3 block text-xs font-medium text-gray-900"
-                            htmlFor="email"
-                        >
-                            Email
-                        </label>
+
                         <div className="relative">
                             <input
                                 // className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
@@ -35,14 +30,15 @@ export default function LoginForm() {
                             />
                             {/* <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" /> */ }
                         </div>
-                    </div>
-                    <div className="mt-4 flex gap-2">
                         <label
-                            className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-                            htmlFor="password"
+                            className="mb-3 mt-5 ml-3 block text-xs font-medium text-gray-900"
+                            htmlFor="email"
                         >
-                            Password
+                            Email
                         </label>
+                    </div>
+                    <div className="mt-4 flex gap-4">
+
                         <div className="relative">
                             <input
                                 // className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
@@ -55,6 +51,12 @@ export default function LoginForm() {
                             />
                             {/* <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" /> */ }
                         </div>
+                        <label
+                            className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                            htmlFor="password"
+                        >
+                            Password
+                        </label>
                     </div>
                 </div>
                 <LoginButton />
