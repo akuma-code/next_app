c5635e0e0307c8ce85b750e1c7d7d97213d98a6f
akuma-code <akumapl86@gmail.com>
2024-05-10 23:07:22 +0300

    sync

--- src/ClientComponents/DateCalendar/EventCalendar.tsx
+++ src/ClientComponents/DateCalendar/EventCalendar.tsx
@@ -1,23 +1,51 @@
 'use client'
 
+import { _log } from "@/Helpers/helpersFns"
 import { Divider } from "@mui/material"
-import { DateCalendar } from "@mui/x-date-pickers"
+import { DateCalendar, DateView } from "@mui/x-date-pickers"
+import { PickerSelectionState } from "@mui/x-date-pickers/internals"
 import dayjs from "dayjs"
-import { useParams } from "next/navigation"
-import { Suspense } from "react"
+import Link from "next/link"
+import { useParams, useSearchParams } from "next/navigation"
+import { Suspense, useCallback, useState } from "react"
 
 
 const EventCalendar = () => {
-    const { y, m, d } = useParams<{ y: string, m: string, d: string }>()
+    const [value, setValue] = useState<any | undefined>()
+    const searchParams = useSearchParams()
+
+    // Get a new searchParams string by merging the current
+    // searchParams with a provided key/value pair
+    const createQueryString = useCallback(
+        (name: string, value: string) => {
+            const params = new URLSearchParams(searchParams.toString())
+            params.set(name, value)
+
+            return params.toString()
+        },
+        [searchParams]
+    )
+
+    const today = dayjs(value)
     return (
         <>
             <Suspense fallback={ <div>load</div> }>
-                { y }/{ m }/{ d }
+
                 <DateCalendar
-                    referenceDate={ dayjs('2024-05-12') }
+                    showDaysOutsideCurrentMonth={ true }
+                    onChange={ (v) => {
+                        _log(dayjs(v))
+                        const date = [+v.$y, +v.$M + 1, +v.$D]
+                        setValue(date.join('-'))
+                        const s = createQueryString('date', value)
+
+                        return s
+                    } }
+
+
                 />
             </Suspense>
-            <Divider></Divider>
+
         </>
     )
 }
--- src/app/avangard/page.tsx
+++ src/app/avangard/page.tsx
@@ -1,8 +1,10 @@
 import EventCalendar from "@/ClientComponents/DateCalendar/EventCalendar";
+type PageProps = {
 
+}
 
 export default async function AvangardPage() {
-
+    console.clear()
 
     return (
         <EventCalendar />
