d11527a4ee4a1a294396f40a6a2f1a27204581c4
akuma-code <akumapl86@gmail.com>
2024-05-24 10:36:13 +0300

    fix pg

--- src/app/avangard/events/page.tsx
+++ src/app/avangard/events/page.tsx
@@ -1,12 +1,6 @@
-import { getPlayers, getPlayersByEventDate } from "@/Services/playerService"
-import { PlayersList } from "../PlayersList"
-import { PlayersEventTranfer } from "@/ClientComponents/PlayersEventTransfer"
-import { getEventsByMonth, getEventsUnique, getEventsWithPlayers } from "@/Services/eventService"
-import { Badge, Box, Stack } from "@mui/material"
-import Link from "next/link"
 import { EventsList } from "@/ClientComponents/EventsList"
-import dayjs from "dayjs"
 import { MonthTabs } from "@/ClientComponents/Tabs/MonthTabs"
+import { getEventsByMonth } from "@/Services/eventService"
 
 const EventsPage = async ({ searchParams }: { searchParams: { date: string, month?: string } }) => {
     const month = searchParams.month
