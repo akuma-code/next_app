d03029aef6e65210f8c543a817ca4cc632663899
akuma-code <akumapl86@gmail.com>
2024-05-29 19:09:39 +0300

    fix admin

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,14 +1,12 @@
 'use server'
 
-import { Auth } from "@/ClientComponents/Auth";
 import { Box } from "@mui/material";
-import Link from "next/link";
 
 function AministratorPage() {
 
     return (
         <Box>
-            <Auth />
+
         </Box>
     );
 }
