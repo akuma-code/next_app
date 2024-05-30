93b49f5a527691ad3f046fa9676b76dba42b38f2
akuma-code <akumapl86@gmail.com>
2024-05-30 08:54:27 +0300

    dev init

--- src/theme.ts
+++ src/theme.ts
@@ -2,6 +2,8 @@
 import { Roboto } from 'next/font/google';
 import { createTheme } from '@mui/material/styles';
 import { ruRU } from '@mui/material/locale'
+import { PaletteMode } from '@mui/material';
+import { amber, deepOrange, grey } from '@mui/material/colors';
 
 const roboto = Roboto({
     weight: ['300', '400', '500', '700'],
@@ -16,5 +18,35 @@ const theme = createTheme({
 
 }, ruRU,
 );
+
+const getDesignTokens = (mode: PaletteMode) => ({
+    palette: {
+        mode,
+        ...(mode === 'light'
+            ? {
+                // palette values for light mode
+                primary: amber,
+                divider: amber[200],
+                text: {
+                    primary: grey[900],
+                    secondary: grey[800],
+                },
+            }
+            : {
+                // palette values for dark mode
+                primary: deepOrange,
+                divider: deepOrange[700],
+                background: {
+                    default: deepOrange[900],
+                    paper: deepOrange[900],
+                },
+                text: {
+                    primary: '#fff',
+                    secondary: grey[500],
+                },
+            }),
+    },
+});
+
 
 export default theme;
