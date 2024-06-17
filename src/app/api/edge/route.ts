2f292ca139d9a24e8e87e60c24d24fad714dcd3e
akuma-code <akumapl86@gmail.com>
2024-06-17 17:20:46 +0300

    remove edge

--- src/app/api/edge/route.ts
+++ src/app/api/edge/route.ts
@@ -1,18 +1,11 @@
+import prisma from '@/client/client'
 import { NextResponse } from 'next/server'
-import { PrismaClient } from '@prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
-import { Pool } from 'pg'
-
 export const runtime = 'edge'
 
-export async function GET(request: Request) {
-    const pool = new Pool({ connectionString: process.env.POSTGRES_PRISMA_URL })
-    const adapter = new PrismaPg(pool)
-    //   const neon = new Pool({ connectionString: process.env.POSTGRES_PRISMA_URL })
-    //   const adapter = new PrismaPg(neon)
-    const prisma = new PrismaClient({ adapter })
+// export async function GET(request: Request) {
 
-    const users = await prisma.user.findMany()
 
-    return NextResponse.json(users, { status: 200 })
-}
+//     const users = await prisma.user.findMany()
+
+//     return NextResponse.json(users, { status: 200 })
+// }
