14ed001ce4061599690c7c83118156382a2590ab
akuma-code <akumapl86@gmail.com>
2024-06-29 01:51:13 +0300

    SYNC

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,125 +1,123 @@
-
-import { PrismaClient } from '@prisma/client';
-import { events_to_seed } from './events';
-import { masters_to_seed, players_to_seed2 } from './players';
-import { members_seed } from './users';
+import { PrismaClient } from "@prisma/client";
+import { events_to_seed } from "./events";
+import { masters_to_seed, players_to_seed2 } from "./players";
+import { members_seed } from "./users";
 
 type SeedEvent = {
+  id: number;
+  date_formated: string;
+  title: string;
+  isDraft?: boolean;
+  players: {
     id: number;
-    date_formated: string;
-    title: string;
-    isDraft?: boolean
-    players: {
-        id: number;
-        name: string;
-        // createdAt: string;
-        // updatedAt: string;
-    }[];
-}
+    name: string;
+    // createdAt: string;
+    // updatedAt: string;
+  }[];
+};
 
-export const prisma = new PrismaClient()
+export const prisma = new PrismaClient();
 
 export async function seedPlayers(seed_names: string[]) {
-    try {
-
-        const seedArray = seed_names.map(s => prisma.player.create({
-            data: { name: s },
-            include: { info: true, events: true }
-        }))
-        return await prisma.$transaction(seedArray)
-    } catch (error) {
-        console.log("___\n", error)
-        throw new Error("SEED ERROR")
-    }
+  try {
+    const seedArray = seed_names.map((s) =>
+      prisma.player.create({
+        data: { name: s },
+        include: { info: true, events: true },
+      })
+    );
+    return await prisma.$transaction(seedArray);
+  } catch (error) {
+    console.log("___\n", error);
+    throw new Error("SEED ERROR");
+  }
 }
-export async function seedObjectPlayers(seed_players: { id: number, name: string }[]) {
-    try {
-
-        const seedArray = seed_players.map(s => prisma.player.create({
-            data: {
-                name: s.name,
-            },
-
-        }))
-        return await prisma.$transaction(seedArray)
-    } catch (error) {
-        console.log("___\n", error)
-        throw new Error("SEED PLAYERS ERROR")
-    }
+export async function seedObjectPlayers(
+  seed_players: { id: number; name: string }[]
+) {
+  try {
+    const seedArray = seed_players.map((s) =>
+      prisma.player.create({
+        data: {
+          name: s.name,
+        },
+      })
+    );
+    return await prisma.$transaction(seedArray);
+  } catch (error) {
+    console.log("___\n", error);
+    throw new Error("SEED PLAYERS ERROR");
+  }
 }
 
 export async function seedEvents(seed_events: SeedEvent[]) {
-    const ev = prisma.event
+  const ev = prisma.event;
 
-    try {
-        // console.log(seed_events[0])
-        const seed = seed_events.map(e => ev.create({
-            data: {
-                date_formated: e.date_formated,
-                title: e.title,
-                id: e.id,
-                isDraft: e.isDraft || false,
+  try {
+    // console.log(seed_events[0])
+    const seed = seed_events.map((e) =>
+      ev.create({
+        data: {
+          date_formated: e.date_formated,
+          title: e.title,
+          id: e.id,
+          isDraft: e.isDraft || false,
+        },
+      })
+    );
 
-            }
-        }))
-
-
-        return await prisma.$transaction(seed)
-    } catch (error) {
-        console.log("\n seed events error \n", error)
-        throw new Error("Restore events error")
-    }
+    return await prisma.$transaction(seed);
+  } catch (error) {
+    console.log("\n seed events error \n", error);
+    throw new Error("Restore events error");
+  }
 }
 
 export async function seedMasters(masters: { name: string }[]) {
-    try {
-        const seed = masters.map(m => prisma.master.create({ data: m }))
+  try {
+    const seed = masters.map((m) => prisma.master.create({ data: m }));
 
-        return await prisma.$transaction(seed)
-    } catch (error) {
-        console.log(error)
-        throw new Error("_____Seed master error")
-    }
-
+    return await prisma.$transaction(seed);
+  } catch (error) {
+    console.log(error);
+    throw new Error("_____Seed master error");
+  }
 }
 
 async function seedUsers() {
-
-
-
-
-    try {
-        const users = members_seed.map(user => prisma.user.create({ data: user }))
-        // const user = prisma.user.create({
-        //     data: { email: admin.email, password: admin.password, role: UserRole.ADMIN, name: admin.name }
-        // })
-        return await prisma.$transaction(users)
-    } catch (e) {
-        console.log(e)
-        throw new Error("Seed admin error")
-    }
+  try {
+    const users = members_seed.map((user) =>
+      prisma.user.create({ data: user })
+    );
+    // const user = prisma.user.create({
+    //     data: { email: admin.email, password: admin.password, role: UserRole.ADMIN, name: admin.name }
+    // })
+    return await prisma.$transaction(users);
+  } catch (e) {
+    console.log(e);
+    throw new Error("Seed admin error");
+  }
 }
 
 export async function seed_db() {
-    const players_seed = seedObjectPlayers(players_to_seed2).finally(console.table)
-    const masters_seed = seedMasters(masters_to_seed).finally(console.table)
-    const events_seed = seedEvents(events_to_seed).finally(console.table)
-    const user_seed = seedUsers().finally(console.table)
-    return Promise.all([
-        players_seed,
-        masters_seed,
-        events_seed,
-        user_seed
-    ]).then(
-        () => console.log("Database seeded succesful"),
-        (e) => console.log("SEED ERROR!", e))
+  const players_seed = seedObjectPlayers(players_to_seed2).finally(
+    console.info
+  );
+  const masters_seed = seedMasters(masters_to_seed).finally(console.table);
+  const events_seed = seedEvents(events_to_seed).finally(console.table);
+  const user_seed = seedUsers().finally(console.table);
+  return Promise.all([players_seed, masters_seed, events_seed, user_seed]).then(
+    () => console.log("Database seeded succesful"),
+    (e) => console.log("SEED ERROR!", e)
+  );
 }
 
-seed_db().then(async () => {
-    await prisma.$disconnect()
-})
-    .catch(async (e) => {
-        console.error(e)
-        await prisma.$disconnect()
-        process.exit(1)
-    })
+seed_db()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
--- src/Services/eventService.ts
+++ src/Services/eventService.ts
@@ -1,52 +1,52 @@
-'use server'
+"use server";
 
-import { _formated_date } from "@/Helpers/dateFuncs"
-import { _log } from "@/Helpers/helpersFns"
-import { revalidatePath } from "next/cache"
-import prisma from "@/client/client"
-import { db_events } from "@/dataStore/backup_db"
+import { _formated_date } from "@/Helpers/dateFuncs";
+import { _log } from "@/Helpers/helpersFns";
+import { revalidatePath } from "next/cache";
+import prisma from "@/client/client";
+import { db_events } from "@/dataStore/backup_db";
 
 export interface EventCreatePayload {
-    event_date: string
-    isDraft: boolean
-    ids: { id: number }[]
-
+    event_date: string;
+    isDraft: boolean;
+    ids: { id: number }[];
 }
 
 export interface DTO_NewEvent {
-    id?: number
-    date_formated: string,
-    title: string,
-    isDraft: boolean
+    id?: number;
+    date_formated: string;
+    title: string;
+    isDraft: boolean;
     players: {
-        id: number,
-        name: string
-    }[],
+        id: number;
+        name: string;
+    }[];
 }
 export interface EventUpdatePayload {
-    id: number
+    id: number;
     _new_data: {
-        date_formated?: string
-        players: { id: number, name: string }[]
-        title?: string | null
-        isDraft?: boolean
-    }
-
+        date_formated?: string;
+        players: { id: number; name: string }[];
+        title?: string | null;
+        isDraft?: boolean;
+    };
 }
 function validateDate(date_to_valid: string) {
-    const regex = /\d{2}_\d{2}_\d{4}/gi
-    const isValid = regex.test(date_to_valid)
-    _log("__date: ", date_to_valid, { isValid })
-    return isValid
+    const regex = /\d{2}_\d{2}_\d{4}/gi;
+    const isValid = regex.test(date_to_valid);
+    _log("__date: ", date_to_valid, { isValid });
+    return isValid;
 }
 
 export async function eventCreate(payload: DTO_NewEvent) {
     const { date_formated, players, title, isDraft = false } = payload;
     try {
-
         const e = await prisma.event.create({
             data: {
-                isDraft, date_formated, title, players: { connect: players.map(p => ({ id: p.id })) }
+                isDraft,
+                date_formated,
+                title,
+                players: { connect: players.map((p) => ({ id: p.id })) },
             },
             select: {
                 id: true,
@@ -55,78 +55,83 @@ export async function eventCreate(payload: DTO_NewEvent) {
                 players: true,
                 isDraft: true,
                 eventInfo: true,
-                pairs: true
-            }
-        })
-        return e
+                pairs: true,
+            },
+        });
+        return e;
     } catch (error) {
-        console.log(" \n", error)
-        throw new Error("Create event error:")
+        console.log(" \n", error);
+        throw new Error("Create event error:");
     } finally {
-        revalidatePath('/')
+        revalidatePath("/");
     }
 }
 
 export async function makeNewEvent(payload: DTO_NewEvent) {
     const { date_formated, players, title, isDraft = false } = payload;
-    const existEvent = await prisma.event.findUnique({ where: { date_formated } })
+    const existEvent = await prisma.event.findUnique({
+        where: { date_formated },
+    });
 
     if (existEvent) {
-        const { id } = existEvent
-        return await updateEventPlayers({ id, _new_data: { players, date_formated, title, isDraft } })
+        const { id } = existEvent;
+        return await updateEventPlayers({
+            id,
+            _new_data: { players, date_formated, title, isDraft },
+        });
     }
 
-    const e = await eventCreate(payload)
-    return e
-
+    const e = await eventCreate(payload);
+    return e;
 }
 
 export async function seedEvents(mock?: typeof db_events) {
-    const ev = prisma.event
-    const eventArray = mock ? mock : db_events.map(e => ({ ...e }))
+    const ev = prisma.event;
+    const eventArray = mock ? mock : db_events.map((e) => ({ ...e }));
     try {
-        const seed = eventArray.map(e => ev.create({
-            data: {
-                date_formated: e.date_formated,
-                title: e.title,
-                id: e.id,
-                players: {
-
-                    connect: e.players.map(p => ({ id: p.id })),
-
+        const seed = eventArray.map((e) =>
+            ev.create({
+                data: {
+                    date_formated: e.date_formated,
+                    title: e.title,
+                    id: e.id,
+                    players: {
+                        connect: e.players.map((p) => ({ id: p.id })),
+                    },
                 },
-            }
-        }))
-        return await prisma.$transaction(seed)
+            })
+        );
+        return await prisma.$transaction(seed);
     } catch (error) {
-        _log("\n seed events error", error)
-        throw new Error("Restore events error")
+        _log("\n seed events error", error);
+        throw new Error("Restore events error");
     }
 }
 
-
 export async function createEvent(payload: EventCreatePayload) {
-    const { event_date, ids } = payload
-    const date = validateDate(event_date) ? event_date : _formated_date(event_date)
+    const { event_date, ids } = payload;
+    const date = validateDate(event_date)
+        ? event_date
+        : _formated_date(event_date);
     // if (!dayjs(event_date).isValid()) {
     //     _log(`${event_date} is wrong!`)
     //     throw new Error(`\n\nEvent date is invalid: ${event_date}`)
     // }
 
-    const existEvent = await prisma.event.findUnique({ where: { date_formated: date } })
+    const existEvent = await prisma.event.findUnique({
+        where: { date_formated: date },
+    });
     if (existEvent) {
         try {
-            const ee = await connectPlayersToEvent(existEvent.id, ids)
+            const ee = await connectPlayersToEvent(existEvent.id, ids);
 
-
-            return ee
+            return ee;
         } catch (error) {
-            console.log(" \n", error)
-            throw new Error("___Update event error:")
+            console.log(" \n", error);
+            throw new Error("___Update event error:");
         } finally {
-            revalidatePath('/')
+            revalidatePath("/");
         }
-
     }
     try {
         const ev = await prisma.event.create({
@@ -135,51 +140,46 @@ export async function createEvent(payload: EventCreatePayload) {
 
                 date_formated: date,
                 players: {
-                    connect: ids
-                }
+                    connect: ids,
+                },
             },
             select: {
                 players: {
                     select: {
                         id: true,
                         name: true,
-
-                    }
+                    },
                 },
                 date_formated: true,
+            },
+        });
 
-
-            }
-        })
-
-
-        _log("\nCreated event: ", ev)
-        return ev
+        _log("\nCreated event: ", ev);
+        return ev;
     } catch (error) {
-        console.log("___Create event error: \n", error)
-        throw new Error("Error while create event")
+        console.log("___Create event error: \n", error);
+        throw new Error("Error while create event");
     } finally {
-        revalidatePath('/')
+        revalidatePath("/");
     }
-
 }
 export async function deleteEvent(id: number) {
     try {
-        const ev = await prisma.event.delete({ where: { id } })
-        _log("deleted event: ", { id })
-        return ev
+        const ev = await prisma.event.delete({ where: { id } });
+        _log("deleted event: ", { id });
+        return ev;
     } catch (error) {
-        console.log("___Delete event error: \n", error)
-        throw new Error("Error while deleting event")
+        console.log("___Delete event error: \n", error);
+        throw new Error("Error while deleting event");
     } finally {
-        revalidatePath('/')
+        revalidatePath("/");
     }
 }
 export async function updateEventPlayers(payload: EventUpdatePayload) {
-    const { id, _new_data } = payload
+    const { id, _new_data } = payload;
 
-    const { title, players, date_formated, isDraft } = _new_data
-    const _players = players.map(p => ({ id: p.id, name: p.name }))
+    const { title, players, date_formated, isDraft } = _new_data;
+    const _players = players.map((p) => ({ id: p.id, name: p.name }));
     try {
         const ev = await prisma.event.update({
             where: { id },
@@ -189,38 +189,37 @@ export async function updateEventPlayers(payload: EventUpdatePayload) {
                 title,
                 players: {
                     set: [],
-                    connect: _players
-                }
-            }
-        })
-        _log('\n', { ev })
-        return ev
+                    connect: _players,
+                },
+            },
+        });
+        _log("\n", { ev });
+        return ev;
     } catch (error) {
-        console.log(" \n", error)
-        throw new Error("___Update event error:")
+        console.log(" \n", error);
+        throw new Error("___Update event error:");
     } finally {
-        revalidatePath('/')
+        revalidatePath("/");
     }
 }
 
 export async function getEventsUnique(date?: string) {
     if (!date) {
-        _log({ date })
-        return null
+        _log({ date });
+        return null;
     }
 
     try {
-        const playersByDate = (date: string) => prisma.event.findUnique({ where: { date_formated: date } }).players()
-        return await playersByDate(date)
+        const playersByDate = (date: string) =>
+            prisma.event.findUnique({ where: { date_formated: date } }).players();
+        return await playersByDate(date);
     } catch (error) {
-        console.log(" \n", error)
-        throw new Error("get event players error:")
+        console.log(" \n", error);
+        throw new Error("get event players error:");
     } finally {
-        revalidatePath('/')
+        revalidatePath("/");
     }
-
 }
-
 
 async function getEventsWithPlayers() {
     const ev = await prisma.event.findMany({
@@ -230,18 +229,22 @@ async function getEventsWithPlayers() {
             date_formated: true,
             id: true,
             players: true,
-            _count: { select: { players: true } }
+            _count: { select: { players: true } },
+        },
+        orderBy: { date_formated: "asc" },
+    });
 
-        }, orderBy: { date_formated: "asc" }
-    })
-
-    return ev
+    return ev;
 }
 
-export async function getEventsByMonth(month?: string, order?: 'asc' | 'desc', year?: number,) {
-    const y = year ? year : 2024
-    const e = prisma.event
-    const _order = order ? order : 'asc'
+export async function getEventsByMonth(
+    month?: string,
+    order?: "asc" | "desc",
+    year?: number
+) {
+    const y = year ? year : 2024;
+    const e = prisma.event;
+    const _order = order ? order : "asc";
     try {
         if (!month) {
             // if month omit return all avents with players
@@ -253,122 +256,125 @@ export async function getEventsByMonth(month?: string, order?: 'asc' | 'desc', y
                     date_formated: true,
                     players: { select: { id: true, name: true } },
                     _count: { select: { players: true } },
-                }
-            })
-            return events
+                },
+            });
+            return events;
         }
-        const searchM = `${month}_${y}` as const
+        const searchM = `${month}_${y}` as const;
         const result = await e.findMany({
             where: {
                 AND: {
                     players: {},
-                    date_formated: { endsWith: searchM }
-                }
+                    date_formated: { endsWith: searchM },
+                },
             },
             select: {
                 id: true,
                 date_formated: true,
                 players: { select: { id: true, name: true } },
-                _count: { select: { players: true } }
+                _count: { select: { players: true } },
             },
-            orderBy: { date_formated: _order }
+            orderBy: { date_formated: _order },
+        });
 
-        })
-
-        return result
+        return result;
     } catch (error) {
-        console.log(" \n", error)
-        throw new Error("Error in function: getEventsByMonth")
+        console.log(" \n", error);
+        throw new Error("Error in function: getEventsByMonth");
     } finally {
-        revalidatePath('/')
+        revalidatePath("/");
     }
-
 }
 
 export async function getEventById(eventId: string) {
-    const id = Number(eventId)
-    if (isNaN(id)) throw new Error(`Event Id Invalid: ${eventId}`)
+    const id = Number(eventId);
+    if (isNaN(id)) throw new Error(`Event Id Invalid: ${eventId}`);
 
-    const e = prisma.event
+    const e = prisma.event;
     try {
         const event = await e.findUnique({
             where: { id },
             select: {
-                id: true, date_formated: true, title: true,
-                players: true, _count: { select: { players: true } },
-                isDraft: true, pairs: true
-            }
-        })
-        return event
+                id: true,
+                date_formated: true,
+                title: true,
+                players: true,
+                _count: { select: { players: true } },
+                isDraft: true,
+                pairs: true,
+            },
+        });
+        return event;
     } catch (error) {
-        console.log(" \n", error)
-        throw new Error("get eventId error:")
+        console.log(" \n", error);
+        throw new Error("get eventId error:");
     }
 }
 export async function getOneEventByDate(date: string) {
-
-
-    const e = prisma.event
+    const e = prisma.event;
     try {
         const event = await e.findUnique({
             where: { date_formated: date },
             select: {
-                id: true, date_formated: true, title: true,
+                id: true,
+                date_formated: true,
+                title: true,
                 players: true,
                 //  _count: { select: { players: true }                  }
-            }
-        })
-        return event
+            },
+        });
+        return event;
     } catch (error) {
-        console.log(" \n", error)
-        throw new Error("get event by date error:")
+        console.log(" \n", error);
+        throw new Error("get event by date error:");
     }
 }
-
 
 export async function createBlankEvent(date: string, title?: string) {
     try {
-        const _date = validateDate(date) ? date : _formated_date(date)
-        const pev = await prisma.event.create({ data: { date_formated: _date, title: title, players: {} }, select: { id: true, date_formated: true } })
-        _log("created event: \n", pev)
-        return pev
-
+        const _date = validateDate(date) ? date : _formated_date(date);
+        const pev = await prisma.event.create({
+            data: { date_formated: _date, title: title, players: {} },
+            select: { id: true, date_formated: true },
+        });
+        _log("created event: \n", pev);
+        return pev;
     } catch (error) {
-        console.log(" \n", error)
-        throw new Error("create blank_event error")
+        console.log(" \n", error);
+        throw new Error("create blank_event error");
     }
 }
 
-export async function connectPlayersToEvent(id: number, players: { id: number }[]) {
-
+export async function connectPlayersToEvent(
+    id: number,
+    players: { id: number }[]
+) {
     try {
-
         const connect_event = await prisma.event.update({
             where: { id },
             data: {
                 players: {
                     set: [],
-                    connect: players
-                }
+                    connect: players,
+                },
             },
             include: {
                 players: {
                     select: {
                         id: true,
-                        name: true
-                    }
-                }
-            }
-        })
+                        name: true,
+                    },
+                },
+            },
+        });
         // _log("\ntsx: ", { connect_event })
-        return connect_event
+        return connect_event;
     } catch (error) {
-        console.log(" \n", error)
-        throw new Error("CONNECT PLAYERS ERROR")
+        console.log(" \n", error);
+        throw new Error("CONNECT PLAYERS ERROR");
     } finally {
-        revalidatePath('/')
+        revalidatePath("/");
     }
-
 }
 
 export async function connectOnePlayer(eventId: number, playerId: number) {
@@ -377,20 +383,19 @@ export async function connectOnePlayer(eventId: number, playerId: number) {
             where: { id: playerId },
             data: {
                 events: {
-                    connect: { id: eventId }
+                    connect: { id: eventId },
                 },
-            }, select: {
-                id: true, name: true
-            }
-
-        })
-        return p
+            },
+            select: {
+                id: true,
+                name: true,
+            },
+        });
+        return p;
     } catch (error) {
-        console.log(error)
-        return null
-    } finally { revalidatePath("/") }
-
-
-
-
+        console.log(error);
+        return null;
+    } finally {
+        revalidatePath("/");
+    }
 }
