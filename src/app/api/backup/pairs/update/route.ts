0e95ea63f5be6bf0df692b5520ef12487e9ae8ae
akuma-code <akumapl86@gmail.com>
2024-08-02 13:40:57 +0300

    fix route

--- src/app/api/backup/pairs/update/route.ts
+++ src/app/api/backup/pairs/update/route.ts
@@ -1,6 +1,6 @@
 import { updatePairs } from "../../events/actions";
 
 export async function GET() {
-    const updater = await updatePairs()
-    return Response.json(updater)
+    await updatePairs()
+    return Response.json("Update complete")
 }
