714784facd602e777ecba2d4b25e8814ab186351
akuma-code <akumapl86@gmail.com>
2024-08-05 19:53:23 +0300

    fix reseed

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,6 @@
 import { AdminCard } from "@/ClientComponents/AdminCardButtons";
 import { Container } from "@mui/material";
-import backup from "./../../../public/json/2024-07-23.json";
+import backup from "./../../../public/json/data.json";
 import { reseedEventsFromJson } from "./actions";
 const action = reseedEventsFromJson.bind(null, backup);
 async function AministratorPage(params: { searchParams: { show: string } }) {
