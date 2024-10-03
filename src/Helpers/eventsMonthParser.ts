3e928c9d9325a008ce5ab020e93a6e12dc9c467b
akuma-code <akumapl86@gmail.com>
2024-10-03 19:31:14 +0300

    fix _date

--- src/Helpers/eventsMonthParser.ts
+++ src/Helpers/eventsMonthParser.ts
@@ -8,15 +8,15 @@ type ReduceResult = {
 export function monthReducer(events?: { date_formated: string; }[]): ReduceResult | null {
     if (!events) return null;
     const res = events.reduce((acc, event) => {
-        const { month: month_name, year, encoded: { d } } = _date(event.date_formated);
+        const { month: month_name, yyyy, dd } = _date(event.date_formated);
 
         const key = month_name as keyof typeof Month;
         const current = acc.months[key];
         if (current) {
             const months = { ...acc.months, [key]: current + 1 };
-            const days = { ...acc.days, [key]: [...acc.days[key], d] }
-            acc = { ...acc, year, months, days };
-        } else acc = { ...acc, year, months: { ...acc.months, [key]: 1 }, days: { ...acc.days, [key]: [d] } };
+            const days = { ...acc.days, [key]: [...acc.days[key], dd] }
+            acc = { ...acc, year: yyyy, months, days };
+        } else acc = { ...acc, year: yyyy, months: { ...acc.months, [key]: 1 }, days: { ...acc.days, [key]: [dd] } };
 
 
         return acc;
